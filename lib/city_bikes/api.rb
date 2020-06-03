3f5342969244f6040fa273b26fa53c0295715ecb
tonitdiep <adiep123@gmail.com>
2020-06-03 18:50:33 -0400

    api update

--- lib/city_bikes/api.rb
+++ lib/city_bikes/api.rb
@@ -8,13 +8,15 @@ class API
         hash = JSON.parse(response)
         array_of_stations = hash["network"]["stations"]   
 
-        array_of_stations[0..100].each do |station_hash| 
+        array_of_stations[0..100].each do {|station_hash|} 
             #1)initialize a new station by only looking at stations of a specific range from [0..100] in this iteration 
             #2)assign attributes by the writer method to decide the station_hash
-            station_instance = Bike.new[:empty_slots, :free_bikes, :name] #initialize objects
-            station_instance.empty_slots = station_hash["empty_slots"]
-            station_instance.free_bikes = station_hash["free_bikes"]
-            station_instance.name = station_hash["name"]
+            station_hash.each = Bike.new[empty_slots:, free_bikes:, name:]
+            # station_instance = Bike.new[:empty_slots, :free_bikes, :name] #initialize objects 
+            #attributes that would be assigned thru station_hash
+            # station_instance.empty_slots = station_hash["empty_slots"]
+            # station_instance.free_bikes = station_hash["free_bikes"]
+            # station_instance.name = station_hash["name"]
         end
     end
 end
